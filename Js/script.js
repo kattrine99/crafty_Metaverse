33dbc245265de92ba9404c3fba94975df1da034e
Ekaterina <kattren990703@gmail.com>
2024-10-05 15:42:29 +0500

    added three new screens

---
+++ Js/script.js
@@ -0,0 +1,8 @@
+const menuIcon = document.getElementById('menu-icon');
+const navList = document.getElementById('nav-list');
+
+
+menuIcon.addEventListener('click', () => {
+
+    navList.classList.toggle('open');
+});
